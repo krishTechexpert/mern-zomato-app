c0af12cc4f655f6c1d56172dc3c3f37f7a0850db
krish <krishcode9@gmail.com>
2024-02-26 23:10:21 +0530

    header and banner added

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
+
+function Header(){
+  return (
+    <header className='border-b-2 border-b-orange-500 py-10'>
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className='text-3xl font-bold tracking-light text-orange-500'>
+          Zomato 
+        </Link>
+        <div className="md:hidden">
+          <MobileNav />
+        </div>
+        <div className="hidden md:block">
+          <MainNav/>
+        </div>
+      </div>
+    </header>
+  )
+}
+export default Header;
